bc34e5148f922cc522f43d256a94d073881e24c4
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-06-25 20:39:26 +0100

    commited socket

--- server/server.js
+++ server/server.js
@@ -1,19 +1,24 @@
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 //load express library
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-console.log(publicPath);
 const port = process.env.PORT || 3000; 
 //console.log(__dirname + '/../public'); //__dirname tels us which directory we are!
 
+var server = http.createServer(app);
 //make app variable to configure our express application
 var app = express();
+
+//how to communicate between outr server and client
+var io = socketIO(server);
 
 //configuring our express static middleware using app.use()
 app.use(express.static(publicPath)); //indirectly making our application homepage to go to the public directory and load data
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`server running on port ${port}`);
 });
