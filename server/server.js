3e03c8f7a9c6a9eef2b7c67eec46f7551370cc24
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-06-26 18:18:11 +0100

    added connection to socket and disconnect

--- server/server.js
+++ server/server.js
@@ -8,13 +8,24 @@ const express = require('express');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000; 
 //console.log(__dirname + '/../public'); //__dirname tels us which directory we are!
-
-var server = http.createServer(app);
 //make app variable to configure our express application
 var app = express();
-
+var server = http.createServer(app);
 //how to communicate between outr server and client
 var io = socketIO(server);
+
+//when a new user is connected, we alert that new user has been connected!
+io.on('connection', (socket) => { 
+	console.log('new user connected!');
+
+	//listen for a disconnecting client when dey leave
+	socket.on('disconnect', () => {
+		console.log('Client from the browser has been disconnected!');
+	})
+
+});
+
+
 
 //configuring our express static middleware using app.use()
 app.use(express.static(publicPath)); //indirectly making our application homepage to go to the public directory and load data
